d004082ce912a8b64791113631a5d95d20a671ca
zhl <826321978@qq.com>
2016-09-18 08:37:26 +0800

    商品搜索按照类别

--- src/com/lbins/myapp/fragment/NearbyFragment.java
+++ src/com/lbins/myapp/fragment/NearbyFragment.java
@@ -5,12 +5,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.text.SpannableString;
 import android.text.format.DateUtils;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.Interpolator;
 import android.widget.*;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.Projection;
+import com.amap.api.maps.model.*;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -40,7 +55,8 @@ import java.util.Map;
 /**
  * Created by zhl on 2016/7/1.
  */
-public class NearbyFragment extends BaseFragment implements View.OnClickListener{
+public class NearbyFragment extends BaseFragment implements View.OnClickListener,AMap.OnMarkerClickListener,AMap.OnInfoWindowClickListener, AMap.OnMarkerDragListener, AMap.OnMapLoadedListener,
+        AMap.InfoWindowAdapter {
     private View view;
     private Resources res;
 
@@ -54,7 +70,13 @@ public class NearbyFragment extends BaseFragment implements View.OnClickListener
     private int pageIndex = 1;
     private static boolean IS_REFRESH = true;
 
-    private LinearLayout headLiner;
+    private RelativeLayout headLiner;
+
+    private MapView mapView;
+    private AMap aMap;
+
+    private MarkerOptions markerOption;
+    private LatLng latlng = new LatLng(36.061, 103.834);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -66,7 +88,8 @@ public class NearbyFragment extends BaseFragment implements View.OnClickListener
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.three_fragment, null);
         res = getActivity().getResources();
-        initView();
+
+        initView(savedInstanceState);
 
         progressDialog = new CustomProgressDialog(getActivity(), "正在加载中",R.anim.custom_dialog_frame);
         progressDialog.setCancelable(true);
@@ -94,13 +117,21 @@ public class NearbyFragment extends BaseFragment implements View.OnClickListener
         }
     }
 
-    private void initView() {
+    private void initView( Bundle savedInstanceState) {
+
+        if(!StringUtil.isNullOrEmpty(MeirenmeibaAppApplication.latStr) && !StringUtil.isNullOrEmpty(MeirenmeibaAppApplication.lngStr)){
+            latlng = new LatLng(Double.valueOf(MeirenmeibaAppApplication.latStr), Double.valueOf(MeirenmeibaAppApplication.lngStr));
+        }
         location = (TextView) view.findViewById(R.id.location);
         location.setOnClickListener(this);
         btn_scan = (ImageView) view.findViewById(R.id.btn_scan);
         keywords = (TextView) view.findViewById(R.id.keywords);
         lstv = (PullToRefreshListView) view.findViewById(R.id.lstv);
-        headLiner = (LinearLayout) LayoutInflater.from(getActivity()).inflate(R.layout.three_header, null);
+        headLiner = (RelativeLayout) LayoutInflater.from(getActivity()).inflate(R.layout.three_header, null);
+        mapView = (MapView) headLiner.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        init();
+        setUpMap();
 
         adapter = new ItemTuijianDianpusAdapter(listsDianpu, getActivity());
 
@@ -245,6 +276,7 @@ public class NearbyFragment extends BaseFragment implements View.OnClickListener
     public void onDestroy() {
         super.onDestroy();
         getActivity().unregisterReceiver(mBroadcastReceiver);
+        mapView.onDestroy();
     }
 
     @Override
@@ -259,4 +291,363 @@ public class NearbyFragment extends BaseFragment implements View.OnClickListener
             break;
         }
     }
+
+
+
+    private void setUpMap() {
+        aMap.setOnMarkerDragListener(this);// 设置marker可拖拽事件监听器
+        aMap.setOnMapLoadedListener(this);// 设置amap加载成功事件监听器
+        aMap.setOnMarkerClickListener(this);// 设置点击marker事件监听器
+        aMap.setOnInfoWindowClickListener(this);// 设置点击infoWindow事件监听器
+        aMap.setInfoWindowAdapter(this);// 设置自定义InfoWindow样式
+        addMarkersToMap();// 往地图上添加marker
+    }
+
+    /**
+     * 在地图上添加marker
+     */
+    public void addMarkersToMap() {
+        // 文字显示标注，可以设置显示内容，位置，字体大小颜色，背景色旋转角度
+        ArrayList<MarkerOptions> markerOptionlst = new ArrayList<MarkerOptions>();
+//        TextView textView= (TextView) view.findViewById(R.id.location_map);
+
+        if(listsDianpu != null){
+            for(ManagerInfo emp:listsDianpu){
+                markerOption = new MarkerOptions();
+                if(!StringUtil.isNullOrEmpty(emp.getLat_company()) && !StringUtil.isNullOrEmpty(emp.getLng_company())){
+                    //用户有会员定位数据
+                    final LatLng latLngTmp = new LatLng(Double.valueOf(emp.getLat_company()), Double.valueOf(emp.getLng_company()));
+                    markerOption.position(latLngTmp);
+
+                    markerOption.title(emp.getCompany_name()).snippet(emp.getCompany_name());
+                    markerOption.draggable(true);
+
+//                    final ImageView imageView = (ImageView) view.findViewById(R.id.location_map);
+//                    imageLoader.displayImage(emp.getMm_emp_cover(), imageView, GuirenApplication.txOptions, new AnimateFirstDisplayListener(){
+//                        @Override
+//                        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+//                            super.onLoadingComplete(imageUri, view, loadedImage);
+//
+//                            Bitmap bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+//                            markerOption.icon(
+//                            BitmapDescriptorFactory.fromBitmap(bitmap));
+//
+//                        }
+//                    });
+                    // 将Marker设置为贴地显示，可以双指下拉看效果
+//                    textView.setText(emp.getMm_emp_nickname());
+//                    textView.setTextSize(8.0f);
+//                    textView.setPadding(3,3,3,6);
+//                    textView.setTextColor(getResources().getColor(R.color.white));
+//                    textView.setDrawingCacheEnabled(false);
+//                    textView.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+//                    textView.layout(0, 0, textView.getMeasuredWidth(), textView.getMeasuredHeight()*2);
+//                    Bitmap bitmap = textView.getDrawingCache();
+//                    markerOption.icon(
+//                            BitmapDescriptorFactory.fromBitmap(bitmap));
+
+                    markerOption.setFlat(true);
+                    markerOption.icon(BitmapDescriptorFactory
+                            .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+                    markerOptionlst.add(markerOption);
+                }
+
+            }
+            List<Marker> markerlst = aMap.addMarkers(markerOptionlst, true);
+        }
+    }
+
+    /**
+     * 对marker标注点点击响应事件
+     */
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        if (aMap != null) {
+            jumpPoint(marker);
+        }
+        return false;
+    }
+
+    /**
+     * 从地上生长效果，实现思路
+     * 在较短的时间内，修改marker的图标大小，从而实现动画<br>
+     * 1.保存原始的图片；
+     * 2.在原始图片上缩放得到新的图片，并设置给marker；
+     * 3.回收上一张缩放后的图片资源；
+     * 4.重复2，3步骤到时间结束；
+     * 5.回收上一张缩放后的图片资源，设置marker的图标为最原始的图片；
+     *
+     * 其中时间变化由AccelerateInterpolator控制
+     * @param marker
+     */
+    private void growInto(final Marker marker) {
+        marker.setVisible(false);
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        final long duration = 250;// 动画总时长
+        final Bitmap bitMap = marker.getIcons().get(0).getBitmap();// BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher);
+        final int width = bitMap.getWidth();
+        final int height = bitMap.getHeight();
+
+        final Interpolator interpolator = new AccelerateInterpolator();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                float t = interpolator.getInterpolation((float) elapsed
+                        / duration);
+
+                if (t > 1) {
+                    t = 1;
+                }
+
+                // 计算缩放比例
+                int scaleWidth = (int) (t * width);
+                int scaleHeight = (int) (t * height);
+                if (scaleWidth > 0 && scaleHeight > 0) {
+
+                    // 使用最原始的图片进行大小计算
+                    marker.setIcon(BitmapDescriptorFactory.fromBitmap(Bitmap
+                            .createScaledBitmap(bitMap, scaleWidth,
+                                    scaleHeight, true)));
+                    marker.setVisible(true);
+
+                    // 因为替换了新的图片，所以把旧的图片销毁掉，注意在设置新的图片之后再销毁
+                    if (lastMarkerBitMap != null
+                            && !lastMarkerBitMap.isRecycled()) {
+                        lastMarkerBitMap.recycle();
+                    }
+
+                    //第一次得到的缩放图片，在第二次回收，最后一次的缩放图片，在动画结束时回收
+                    ArrayList<BitmapDescriptor> list = marker.getIcons();
+                    if (list != null && list.size() > 0) {
+                        // 保存旧的图片
+                        lastMarkerBitMap = marker.getIcons().get(0).getBitmap();
+                    }
+
+                }
+
+                if (t < 1.0 && count < 10) {
+                    handler.postDelayed(this, 16);
+                } else {
+                    // 动画结束回收缩放图片，并还原最原始的图片
+                    if (lastMarkerBitMap != null
+                            && !lastMarkerBitMap.isRecycled()) {
+                        lastMarkerBitMap.recycle();
+                    }
+                    marker.setIcon(BitmapDescriptorFactory.fromBitmap(bitMap));
+                    marker.setVisible(true);
+                }
+            }
+        });
+    }
+
+    /**
+     * marker点击时跳动一下
+     */
+    public void jumpPoint(final Marker marker) {
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        Projection proj = aMap.getProjection();
+        //todo
+        Point startPoint = proj.toScreenLocation(marker.getPosition());
+        startPoint.offset(0, -100);
+        final LatLng startLatLng = proj.fromScreenLocation(startPoint);
+        final long duration = 1500;
+
+        final Interpolator interpolator = new BounceInterpolator();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                float t = interpolator.getInterpolation((float) elapsed
+                        / duration);
+                double lng = t * marker.getPosition().longitude + (1 - t)
+                        * startLatLng.longitude;
+                double lat = t * marker.getPosition().latitude + (1 - t)
+                        * startLatLng.latitude;
+//                marker.setPosition(new LatLng(lat, lng));
+                if (t < 1.0) {
+                    handler.postDelayed(this, 16);
+                }
+            }
+        });
+    }
+
+    private int count = 1;
+    Bitmap lastMarkerBitMap = null;
+
+
+    /**
+     * 监听点击infowindow窗口事件回调
+     */
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+//        Toast.makeText(getActivity(), "你点击了infoWindow窗口"+marker.getTitle() ,Toast.LENGTH_SHORT).show();
+        ManagerInfo empT = null;
+        for(ManagerInfo emp:listsDianpu){
+            if(emp.getCompany_name().equals(marker.getTitle())){
+                LatLng latLng = marker.getPosition();
+                if(String.valueOf(latLng.latitude).equals(emp.getLat_company())){
+                    empT = emp;
+                    break;
+                }
+            }
+        }
+        if(empT != null){
+//            Intent detail = new Intent(getActivity(), ProfileActivity.class);
+//            detail.putExtra("mm_emp_id", empT.getMm_emp_id());
+//            startActivity(detail);
+        }
+
+    }
+
+    /**
+     * 监听拖动marker时事件回调
+     */
+    @Override
+    public void onMarkerDrag(Marker marker) {
+        String curDes = marker.getTitle() + "拖动时当前位置:(lat,lng)\n("
+                + marker.getPosition().latitude + ","
+                + marker.getPosition().longitude + ")";
+//        markerText.setText(curDes);
+    }
+
+    /**
+     * 监听拖动marker结束事件回调
+     */
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+//        markerText.setText(marker.getTitle() + "停止拖动");
+    }
+
+    /**
+     * 监听开始拖动marker事件回调
+     */
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+//        markerText.setText(marker.getTitle() + "开始拖动");
+    }
+
+    /**
+     * 监听amap地图加载成功事件回调
+     */
+    @Override
+    public void onMapLoaded() {
+        // 设置所有maker显示在当前可视区域地图中
+//        LatLngBounds bounds = new LatLngBounds.Builder()
+//                .include(Constants.XIAN).include(Constants.CHENGDU)
+//                .include(Constants.BEIJING).include(latlng).build();
+//        LatLngBounds bounds = new LatLngBounds.Builder()
+//                .include(latlng)
+//                .build();
+//        aMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 150));
+        LatLng marker1 = new LatLng(45, 108);
+        //设置中心点和缩放比例
+        aMap.moveCamera(CameraUpdateFactory.changeLatLng(marker1));
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(2));
+    }
+
+    /**
+     * 监听自定义infowindow窗口的infocontents事件回调
+     */
+    @Override
+    public View getInfoContents(Marker marker) {
+//        if (radioOption.getCheckedRadioButtonId() != R.id.custom_info_contents) {
+//            return null;
+//        }
+        View infoContent = getActivity().getLayoutInflater().inflate(
+                R.layout.custom_info_contents, null);
+        render(marker, infoContent);
+        return infoContent;
+    }
+
+    /**
+     * 监听自定义infowindow窗口的infowindow事件回调
+     */
+    @Override
+    public View getInfoWindow(Marker marker) {
+//        if (radioOption.getCheckedRadioButtonId() != R.id.custom_info_window) {
+//            return null;
+//        }
+        View infoWindow = getActivity().getLayoutInflater().inflate(
+                R.layout.custom_info_window, null);
+
+        render(marker, infoWindow);
+        return infoWindow;
+    }
+
+    /**
+     * 自定义infowinfow窗口
+     */
+    public void render(Marker marker, View view) {
+//        if (radioOption.getCheckedRadioButtonId() == R.id.custom_info_contents) {
+//            ((ImageView) view.findViewById(R.id.badge))
+//                    .setImageResource(R.drawable.badge_sa);
+//        } else if (radioOption.getCheckedRadioButtonId() == R.id.custom_info_window) {
+//            ImageView imageView = (ImageView) view.findViewById(R.id.badge);
+//            imageView.setImageResource(R.drawable.badge_wa);
+//        }
+        String title = marker.getTitle();
+        TextView titleUi = ((TextView) view.findViewById(R.id.title));
+        if (title != null) {
+            SpannableString titleText = new SpannableString(title);
+            titleText.setSpan(new ForegroundColorSpan(Color.BLACK), 0,
+                    titleText.length(), 0);
+            titleUi.setTextSize(14);
+            titleUi.setText(titleText);
+
+        } else {
+            titleUi.setText("");
+        }
+        String snippet = marker.getSnippet();
+        TextView snippetUi = ((TextView) view.findViewById(R.id.snippet));
+        if (snippet != null) {
+            SpannableString snippetText = new SpannableString(snippet);
+            snippetText.setSpan(new ForegroundColorSpan(Color.DKGRAY), 0,
+                    snippetText.length(), 0);
+            snippetUi.setTextSize(14);
+            snippetUi.setText(snippetText);
+        } else {
+            snippetUi.setText("");
+        }
+    }
+
+    /**
+     * 初始化AMap对象
+     */
+    private void init() {
+        if (aMap == null) {
+            aMap = mapView.getMap();
+        }
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+
 }
